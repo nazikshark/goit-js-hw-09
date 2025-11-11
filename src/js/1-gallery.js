9b3856269fd7cc74fe456ce0e1f82ef056175feb
nazikshark <nazarpro783@gmail.com>
2025-11-11 16:25:41 +0100

    fggyg

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -82,3 +82,5 @@ new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
